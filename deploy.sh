8700d824ffeca231814ef915c7482e6c0a969262
Clark Green <clark_green@byu.edu>
2018-03-16 18:50:31 -0600

    testing deployment to AWS

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,21 @@ BRANCH=$3
 EB_BUCKET=elasticbeanstalk-us-west-2-194925301021
 
 # Create new Elastic Beanstalk version
+echo $BRANCH
 aws configure set default.region us-west-2
 aws configure set region us-west-2
 aws s3 cp Dockerrun.aws.json s3://$EB_BUCKET/Dockerrun.aws.json # Copy the Dockerrun file to the S3 bucket
 
-if [ "$BRANCH"=="production" ]; then
+#if [ "$BRANCH"=="production" ]; then
+#
+#	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=Dockerrun.aws.json
+#
+#	# Update Elastic Beanstalk environment to new version
+#	aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1
+#
+#elif [ "$BRANCH"=="master" ]; then
 
-	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=Dockerrun.aws.json
-
-	# Update Elastic Beanstalk environment to new version
-	aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1
-
-elif [ "$BRANCH"=="master" ]; then
+if [ "$BRANCH"=="master" ]; then 
 
 	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME-stage --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=Dockerrun.aws.json
 
