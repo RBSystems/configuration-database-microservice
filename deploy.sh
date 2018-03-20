a5886244090f9796e83ee1d2d236aae69051d5e1
Clark Green <clark_green@byu.edu>
2018-03-20 17:56:50 -0600

    trying to fix AWS instance

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,20 @@ EB_BUCKET=elasticbeanstalk-us-west-2-194925301021
 
 # Create new Elastic Beanstalk version
 echo $BRANCH
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json > $DOCKERRUN_FILE
 aws configure set default.region us-west-2
 aws configure set region us-west-2
-aws s3 cp Dockerrun.aws.json s3://$EB_BUCKET/Dockerrun.aws.json # Copy the Dockerrun file to the S3 bucket
+aws s3 cp Dockerrun.aws.json s3://$EB_BUCKET/$DOCKERRUN_FILE
 
-#if [ "$BRANCH"=="production" ]; then
-#
-#	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=Dockerrun.aws.json
-#
-#	# Update Elastic Beanstalk environment to new version
-#	aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1
-#
-#elif [ "$BRANCH"=="master" ]; then
+if [ "$BRANCH"=="production" ]; then
 
-if [ "$BRANCH"=="master" ]; then 
+	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=Dockerrun.aws.json
+
+	# Update Elastic Beanstalk environment to new version
+	aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1
+
+elif [ "$BRANCH"=="master" ]; then
 
 	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME-stage --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=Dockerrun.aws.json
 
