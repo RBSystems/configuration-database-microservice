a959d5bd7d6d007d1a036ede329169bab168b3d7
Clark Green <clark_green@byu.edu>
2018-03-22 11:11:55 -0600

    testing deployments

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ EB_BUCKET=elasticbeanstalk-us-west-2-194925301021
 # Create new Elastic Beanstalk version
 echo $BRANCH
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json > $DOCKERRUN_FILE
+sed "s/<TAG>/$BRANCH/" < Dockerrun.aws.json > $DOCKERRUN_FILE
 aws configure set default.region us-west-2
 aws configure set region us-west-2
 aws s3 cp Dockerrun.aws.json s3://$EB_BUCKET/$DOCKERRUN_FILE
