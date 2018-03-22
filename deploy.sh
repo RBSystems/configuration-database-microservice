316c7c886b0c48cfd03e7659b46ee8284aada9cb
Clark Green <clark_green@byu.edu>
2018-03-22 12:35:41 -0600

    still having issues

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,17 @@ DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$BRANCH/" < Dockerrun.aws.json > $DOCKERRUN_FILE
 aws configure set default.region us-west-2
 aws configure set region us-west-2
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 
 if [ "$BRANCH"=="production" ]; then
 
+	aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE # copy the file that beanstalk needs to spin up the containers into beanstalk's s3 bucket
+	# define new application version
 	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
-
-	# Update Elastic Beanstalk environment to new version
-	aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1
+	aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1 # update environment to new version
 
 elif [ "$BRANCH"=="stage" ]; then
 
+	aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE # copy the file that beanstalk needs to spin up the containers into beanstalk's s3 bucket
 	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME-stage --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
-
-	# Update Elastic Beanstalk environment to new version
 	aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-stg --version-label $SHA1
 fi
