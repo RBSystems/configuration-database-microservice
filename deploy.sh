2a857ad523e2b97577c3b22b75ac9b4404ff7ade
Clark Green <clark_green@byu.edu>
2018-03-22 13:05:25 -0600

    it was a bash issue

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,14 @@ sed "s/<TAG>/$BRANCH/" < Dockerrun.aws.json > $DOCKERRUN_FILE
 aws configure set default.region us-west-2
 aws configure set region us-west-2
 
-if [ "$BRANCH"=="production" ]; then
+if [ "$BRANCH" == "production" ]; then
 
 	aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE # copy the file that beanstalk needs to spin up the containers into beanstalk's s3 bucket
 	# define new application version
 	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 	aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1 # update environment to new version
 
-elif [ "$BRANCH"=="stage" ]; then
+elif [ "$BRANCH" == "stage" ]; then # do the same thing, to the stage environment
 
 	aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE # copy the file that beanstalk needs to spin up the containers into beanstalk's s3 bucket
 	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME-stage --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
