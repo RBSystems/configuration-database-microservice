5d413b19ff3eb2782db80027e0f33367c123d668
Clark Green <clark_green@byu.edu>
2018-03-22 12:23:21 -0600

    still updating erroneously

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$BRANCH/" < Dockerrun.aws.json > $DOCKERRUN_FILE
 aws configure set default.region us-west-2
 aws configure set region us-west-2
-aws s3 cp Dockerrun.aws.json s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 
 if [ "$BRANCH"=="production" ]; then
 
